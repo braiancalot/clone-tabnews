68f55655b2e7b80decf77db76365e48afe710d19
Braian dos Santos Calot <calot665@hotmail.com>
2023-11-28 23:10:56 +0000

    adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+function Home() {
+    return <div style={{ alignItems: "center", textAlign: "center"}}>
+        <h2>Parabéns!!</h2>
+        <br />
+        <p>Se você se chama Naara, mais conhecida como amor da minha vida, você acaba de ganhar um:</p>
+        <br />
+        <h1>VALE SUSHI</h1>
+        <br />
+        <h5><i>Resgate no final de semana mais próximo.</i></h5>
+    </div>
+}
+
+export default Home;
