362a9ccef4722eb5280d45efa64797c57768805d
braiancalot <calot665@hotmail.com>
2026-08-08 06:15:27 -0300

    feat: use `DefaultLayout` on home page

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,15 @@
+import DefaultLayout from "interface/DefaultLayout/index.js";
+
 function Home() {
   return (
-    <div style={{ alignItems: "center", textAlign: "center" }}>
-      <h1>Clone TabNews</h1>
-      {/* <h2>Parabéns!!</h2>
-      <br />
-      <p>
-        Se você se chama Naara, mais conhecida como amor da minha vida, acaba de
-        ganhar um:
-      </p>
-      <br />
-      <h1>VALE SUSHI</h1>
-      <br />
-      <h5>
-        <i>Resgate no final de semana mais próximo.</i>
-      </h5> */}
-    </div>
+    <DefaultLayout
+      metadata={{
+        description:
+          "Conteúdos para quem trabalha com Programação e Tecnologia",
+      }}
+    >
+      <h1>Conteúdos para quem trabalha com Programação e Tecnologia</h1>
+    </DefaultLayout>
   );
 }
 
